6eb341a086a252bedfba7f9cab507491728d4f78
Nathan Wang <nn2wg2@gmail.com>
2021-01-31 04:15:02 +0000

    everything

---
+++ main.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+
+#s = pd.Series([1 ,3 ,5 , np.nan,6 , 8])
+#dates = pd.date_range("20130101", periods=6)
+
+#print(s)
+sioData = 
+#data = pd.read_csv("all_stocks_5yr.csv", dtype = ["date":"string", "open":double, "high":double,"low":double,"close":double,"volume":double,"Name":"string"])
+
+#data = pd.read_csv("https://repl.it/@Notme22224/stock-neural-net#all_stocks_5yr.csv")
+
+data.head()
